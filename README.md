54cf0bebfa0ca5d835862c36c516a3ba98faf687
Flora <afzflora@gmail.com>
2023-09-15 21:39:30 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # weather-bear
+
+### A neat widget that tells you what to wear based off the weather~
+###### A display of outfits will be shown. 
+
+### Set your location
+###### Your teddy bear will be mindful, regardless of where you are at.
+
+### Answer questions about sweater weather~
+###### User may have the opportunity to chose a comfortable temperature of when to wear a sweater outside.
+
+### Don't forget an umbrella when you head out the door!
+###### Based on predictions on forecast!
---
+++ weather-bear.py
@@ -0,0 +1,34 @@
+import requests  
+import flet as ft
+
+set_toCelsius = False
+raining_orSnowing = False
+sunny = False
+cuurent_temperature = 
+
+def obtain_user_data():
+    def ask_user_location():
+        pass
+
+    def ask_mode(current_temperature):     # chilly or warm, ask user for feedback on weather (use sweater weather as a metric)
+        
+        pass
+
+    def ask_CorF():     # celsius or fahrenheit 
+        
+        pass
+
+    def obtain_location():
+        pass
+    
+
+
+def main(page: ft.Page):
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
